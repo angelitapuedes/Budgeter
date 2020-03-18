2880ea5b73626120d516939470df365883966e67
angiemairrodriguez <46510388+angiemairrodriguez@users.noreply.github.com>
2020-03-18 10:47:05 -0400

    Add files via upload
    This is just a skeleton, I want the user to type in his or her amount they Made on their paycheck.

---
+++ BudgetMaster.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+
+public class BudgetMaster
+{
+    int a;
+    int b;
+
+    public static void main(final String[] args)
+    {
+        System.out.println("Write out the amount you made:");
+        System.out.println("");
+    
+
+        
+        
+}
