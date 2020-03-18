975e3212d8b2975595c89fd9dc0871895e3722f4
angiemairrodriguez <46510388+angiemairrodriguez@users.noreply.github.com>
2020-03-18 10:53:43 -0400

    Final Result
    Flaot works Better when your calculating percents to allocate for your budget, so the variables are now float.

--- BudgetMaster.java
+++ BudgetMaster.java
@@ -12,31 +12,35 @@ public class BudgetMaster
         System.out.println("Write out the amount you made:");
         System.out.println("");
 
-         /************** Allocation Begins *******************/
 
-         double tithe = amount * .012;
-         double bills=300;
-         double newAmount=amount-tithe-bills;
- 
-         double saveIngs=newAmount*.20;
-         double blow=newAmount*.05;
- 
-         doube reSt=newAmount-savIngs-blow;
- 
-         /******************Allocation COmplete***************** */
-        if(reSt<=0)
-        {
+
+        float amount = Float.parseFloat(System.console().readLine());
+
+
+        /************** Allocation Begins *******************/
+
+        float tithe = amount * 0.012f;
+        float bills=300.0f;
+        float newAmount=amount-tithe-bills;
+
+        float saveIngs=newAmount*0.20f;
+        float blow=newAmount*0.05f;
+
+        float reSt=newAmount-saveIngs-blow;
+
+        if(reSt<=0){
             System.out.println("Your Under");
         }
-        else
-        {
+        else{
             System.out.println("You did great for this payroll");
         }
         System.out.println("Breakdown of you Budget:");
-        System.out.printf("tithe: %d\n bills: %d\n saveIngs %d\n blow %s\n reSt %d\n",tithe, bills, saveIngs, blow, reSt);
+        System.out.printf("tithe: %f\n bills: %f\n saveIngs %f\n blow %f\n reSt %f\n",tithe, bills, saveIngs, blow, reSt);
 
-    
+    }
 
-        
-        
+    public static double pi()
+    {
+        return 3.14;
+    }
 }
